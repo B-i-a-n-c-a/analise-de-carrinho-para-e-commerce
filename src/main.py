a63438901f8cf463ffa3ac479885c1d1c5e3ab01
B-i-a-n-c-a <biancaft51@gmail.com>
2026-03-23 18:23:18 -0300

    pasta de analises adicionado, que contém as principais metricas possiveis de serem retiradas das tabelas do banco de dados

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os
 from config_bd.dados_banco import preenche_banco, cria_tabela
+from analises.metricas_carrinho import marca_frequente
 
 if __name__ == "__main__":
-    preenche_banco()
+    marca_frequente()
     
 
 
